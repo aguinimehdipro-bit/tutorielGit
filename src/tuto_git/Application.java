93fb5cbf03cf8f1ddf531f450634dc0e7f68c31e
george-SARA <georgesara861@email.com>
2026-01-29 10:14:17 +0100

    ajout: sortie de l'app à la fermeture de la fenêtre

--- src/tuto_git/Application.java
+++ src/tuto_git/Application.java
@@ -1,8 +1,10 @@
 package tuto_git;
 
 import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Application extends Frame {
+public class Application extends Frame implements WindowListener {
 
 	public static void main(String[] args) {
 		new Application();
@@ -20,6 +22,46 @@ public class Application extends Frame {
 		this.add(vue);
 		this.pack();
 		this.setVisible(true);
+		this.addWindowListener(this);
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'windowOpened'");
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'windowIconified'");
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'windowDeiconified'");
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'windowActivated'");
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'windowDeactivated'");
 	}
 
 }
